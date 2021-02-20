38bcb40a0c073222895eb764e9fc3939d8c9f059
Matt <mattfsewell@gmail.com>
2021-02-20 18:06:49 -0500

    commit tree 227c109e143445dcd9c1a2c08a32c4c32a41ebd6

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+function mapToNegativize(array) {
+    let newarray = [];
+    for (var i = 0; i < array.length; i++) {
+        newarray.push(array[i] * -1);
+    }
+    return newarray;
+}
+function mapToNoChange(array) {
+    let newarray = [];
+    for (var i = 0; i < array.length; i++) {
+        newarray.push(array[i]);
+    }
+    return newarray;
+}
+function mapToDouble(array) {
+    let newarray = [];
+    for (var i = 0; i < array.length; i++) {
+        newarray.push(array[i] * 2);
+    }
+    return newarray;
+}
+function mapToSquare(array) {
+    let newarray = [];
+    for (var i = 0; i < array.length; i++) {
+        newarray.push(array[i] * array[i]);
+    }
+    return newarray;
+}
+function reduceToTotal(array, starting=0){
+    let total = starting
+
+    for (var i = 0; i < array.length; i++){
+        total += array[i];
+    }
+    return total
+}
+function reduceToAllTrue(array){
+    for (var i = 0; i < array.length; i++){
+        if (!array[i]){
+            return false;
+        }
+
+    }
+    return true
+}
+function reduceToAnyTrue(array){
+    for (var i = 0; i < array.length; i++){
+        if (array[i]){
+            return true;
+        }
+
+    }
+    return false
+}
